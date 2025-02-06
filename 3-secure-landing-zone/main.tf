8f3dc48f2bf9cd7bd90dcd3037e476b481ea5535
AnushaG66 <anusha.goriparthi@66degrees.com>
2025-02-06 16:16:40 +0530

    added slz

---
+++ 3-secure-landing-zone/main.tf
@@ -0,0 +1,72 @@
+module "folders" {
+  source                    = "terraform-google-modules/folders/google"
+  version                   = "~> 4.0"
+
+  for_each                  = var.folders
+  parent                    = each.value.org
+  names                     = each.value.folder_name
+}
+
+module "shared_services_sub_folders" {
+  source                    = "terraform-google-modules/folders/google"
+  version                   = "~> 4.0"
+
+  parent                    = module.folders["shared"].id
+  names                     = var.shared_sub_folders
+}
+
+module "rd_sub_folders" {
+  source                    = "terraform-google-modules/folders/google"
+  version                   = "~> 4.0"
+
+  parent                    = module.folders["rd"].id
+  names                     = var.rd_sub_folders
+}
+
+module "portfolio_sub_folders" {
+  source                    = "terraform-google-modules/folders/google"
+  version                   = "~> 4.0"
+
+  parent                    = module.folders["portfolio"].id
+  names                     = var.portfolio_sub_folders
+}
+
+module "admin_sub_folders" {
+  source                    = "terraform-google-modules/folders/google"
+  version                   = "~> 4.0"
+
+  parent                    = module.folders["admin"].id
+  names                     = var.admin_sub_folders
+}
+
+module "nonprod_host_project" {
+  source                    = "../modules/host-project-factory"
+  org_id                    = var.org_id
+  projectName               = var.projectName["nonprod_host"]
+  random_project_id         = false
+  labels                    = var.nonprod_host_proj_labels
+  apis                      = var.apis
+  folder_id                 = module.IT_sub_folders.folders_map["non-prod-fldr"].id
+  billing_account           = var.billing_account
+  host_network              = var.nonprod_host_network
+  customRules               = var.firewall_nonprod
+  address                   = var.address
+  cloud-nat                 = var.cloud_nat_nonprod
+  routes                    = var.routes_nonprod
+}
+
+module "prod_host_project" {
+  source                    = "../modules/host-project-factory"
+  org_id                    = var.org_id
+  projectName               = var.projectName["prod_host"]
+  random_project_id         = false
+  labels                    = var.prod_host_proj_labels
+  apis                      = var.apis
+  folder_id                 = module.IT_sub_folders.folders_map["non-prod-fldr"].id
+  billing_account           = var.billing_account
+  host_network              = var.prod_host_network
+  customRules               = var.firewall_prod
+  address                   = var.address
+  cloud-nat                 = var.cloud_nat_prod 
+  routes                    = var.routes_prod
+}
---
+++ 3-secure-landing-zone/secure-landing-zone/main.tfvars
@@ -0,0 +1,26 @@
+folders = {
+  shared = {
+    org           = "organizations/417981694507"
+    folder_name   = ["paradigm-gcp-shared-services"]
+  }
+  rd = {
+    org           = "organizations/417981694507"
+    folder_name   = ["research-and-development"]
+  }
+  portfolio = {
+    org           = "organizations/417981694507"
+    folder_name   = ["portfolio-mgmt"]
+  },
+  admin = {
+    org           = "organizations/417981694507"
+    folder_name   = ["admin"]
+  }
+}
+
+shared_sub_folders            = ["paradigm-ss-nonprod", "paradigm-ss-prod"]
+rd_sub_folders                = ["rd-dev"]
+portfolio_sub_folders         = ["portfolio-mgmt-dev"]
+admin_sub_folders             = ["admin-prod"]
+
+billing_account = "01FDCB-EB1BB1-59378C"
+org_id          = "417981694507"
